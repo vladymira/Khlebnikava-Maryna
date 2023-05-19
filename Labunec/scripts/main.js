591e8b4f4f97e66f73b3366f1040af8de63155cd
Maryna Khlebnikava <vladymira.by@gmail.com>
2023-05-19 12:04:54 +0300

    labunec_main_fix_19052023

--- Labunec/scripts/main.js
+++ Labunec/scripts/main.js
@@ -1,25 +1,33 @@
-$( function() {
-    $( "#tabs" ).tabs();
-  } );
+$(function () {
+  $("#tabs").tabs();
+});
 
 // -- Initialize Swiper -//
-  window.addEventListener("load", (event) => {
-    var swiper = new Swiper(".mySwiper", {
-        loop: true,
-        spaceBetween: 10,
-        slidesPerView: 6,
-        freeMode: true,
-        watchSlidesProgress: true,
-    });
-    var swiper2 = new Swiper(".mySwiper2", {
-        loop: true,
-        spaceBetween: 50,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        thumbs: {
-            swiper: swiper,
-        },
-    });
-});
+window.addEventListener("load", (event) => {
+  var swiper = new Swiper(".mySwiper", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 6,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+
+  var swiper2 = new Swiper(".mySwiper2", {
+    loop: true,
+    spaceBetween: 50,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {
+      swiper: swiper,
+    },
+  });
+
+  var swiper3 = new Swiper(".mySwiper3", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+})
