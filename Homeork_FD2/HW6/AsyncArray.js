f0af2b9da76be80ae996f496e2de6e5a4f4a5dda
Marina Khlebnikova <vladymira.by@gmail.com>
2023-10-31 17:41:49 +0300

    HW6_AsyncArray

--- Homeork_FD2/HW6/AsyncArray.js
+++ Homeork_FD2/HW6/AsyncArray.js
@@ -1,65 +1,20 @@
-// Домашнее задание №6
-// ===================
+class AsyncArray extends Array {
+    constructor(...args) {
+        super(...args);
+    }
 
-// ```
-// Вносите изменения в файлы:
-
-// - ./AsyncArray.js
-// - ./async.js
-// ```
-
-// > В данном домашнем задании нельзя использовать синтаксис `async/await`!
-
-// ## Задание №1
-
-// Создайте класс `AsyncArray` расширяющий обычный `Array`.
-// Класс `AsyncArray` должен содержать один метод `serialMap()`.
-// Данный метод должен работать как обычный `map()`, но с учётом того,
-// что каждое преобразование элемента массива происходит асинхронно,
-// т.е. функция-преобразователь будет возвращать `Promise`.
-// Следовательно, результатом `serialMap()` тоже должен быть `Promise` с новым экземпляром `AsyncArray` содержащим результаты всех асинхронных преобразований.
-// Каждое следующее преобразование должно запускаться только после того, как завершилось предыдущее.
-
-// #### Пример:
-// ```javascript
-// import {AsyncArray} from './AsyncArray.mjs';
-
-// const asyncTransformation = (el, index, asyncArray) => new Promise((resolve, reject) => {
-//     // любое асинхронное преобразование
-// });
-// const asyncArray = new AsyncArray(1, 2, 3);
-
-// asyncArray.serialMap(asyncTransformation).then(result => {
-//     // result - экземпляр AsyncArray с результатами преобразований
-// });
-// ```
+    serialMap(callback) {          
+        return Promise.all(
+            this.map(callback)
+        );
+    }
+}
 
 const asyncArray = new AsyncArray(1, 2, 3);
 
-const asyncTransformation = (el, index, asyncArray) => new Promise((resolve, reject) => {
-    new Promise((resolve, reject) => setTimeout(() => resolve(el+100), 1000))
-     });
+const asyncTransformation = (el, index, asyncArray) => new Promise((resolve, reject) => setTimeout(() => resolve(el + 100), 3000));
 
 
-class AsyncArray extends Array {
-    #newArr;
-
-    constructor(...data)
-    {
-        super();
-        this.newArr = data;
-    }
-
-    get newArr(){
-        return this.#newArr
-    }
-
-    set newArr(value){
-        this.#newArr = value;
-    }
-
-    [Symbol.iterator](){
-        asyncTransformation();
-    }
-  }
-  
+ asyncArray.serialMap(asyncTransformation).then(result => {
+       console.log(result);               
+    });
