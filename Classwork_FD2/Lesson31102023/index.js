7c55ec60f5ca528985402a0ef6c3b85e1649a90b
Marina Khlebnikova <vladymira.by@gmail.com>
2023-11-09 18:33:59 +0300

    add_classwork_31102023

---
+++ Classwork_FD2/Lesson31102023/index.js
@@ -0,0 +1,5 @@
+import { displayUserName, getUserName } from "./utils.js";
+
+const userName = getUserName();
+
+displayUserName(userName);
---
+++ Classwork_FD2/Lesson31102023/utils.js
@@ -0,0 +1,7 @@
+export function getUserName() {
+    return prompt('Enter your name:')
+}
+
+export function displayUserName(userName) {
+    alert(userName);
+}
